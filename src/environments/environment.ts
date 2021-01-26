e7362a9b2d7a2ead8ddfe5959e655913ac71c825
dayana <dayanalily22@gmail.com>
2021-01-26 19:15:07 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: true,
+  apiUrl: 'http://gateway.marvel.com/v1/public/',
+  apiKey: '871c1e539db32ae28978782bc2ef689f4d9bc490', //privateKey 
+  hash: '6ec42540441ad6e235dd34c0b3b08c5e', // publicKey
+  settings: {
+    appErrorPrefix: '[Angular Heroes Error] ',
+    appTitle: 'Angular Heroes',
+    version: '1.0.0'
+  }
+};
+
